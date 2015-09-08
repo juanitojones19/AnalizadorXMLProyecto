7efbe6b1dca87fbd48e9041c21d3717692eae337
juanitojones19 <hastarm@gmail.com>
2015-09-08 13:52:10 -0500

    Clase Lista con el metodo llenarLista y un constructor Lista

--- AnalizadorXML/src/com/repaso/Analizador/Lista.java
+++ AnalizadorXML/src/com/repaso/Analizador/Lista.java
@@ -4,42 +4,34 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Sintelti on 08/09/2015.
  */
 public class Lista
 {
-    String texto;
+    private List<String> lista = new ArrayList<String>();
+    private String texto;
 
-    public Lista(String ruta)
+    public Lista(String texto)
     {
-
-        try
+        String[] lista = llenarLista(texto);
+        for (int i = 0; i < lista.length; i++)
         {
-            int caracter;
-            String letras;
-            texto = "";
-            FileReader f = new FileReader(ruta);
-            BufferedReader b = new BufferedReader(f);
-            while((caracter = b.read())!= -1) {
-                //System.out.println(caracter);
-                letras = "" + (char)caracter;
-                texto += letras;
-                //System.out.print(texto);
-            }
-            b.close();
-        }catch(FileNotFoundException error){
-            error.printStackTrace();
-
-        }catch (IOException error){
-            error.printStackTrace();
+            System.out.println(lista[i]);
         }
-
-
     }// fin del constructor
-    public String obtenerTexto(){
-        return texto;
+
+    public String[] llenarLista(String texto)
+    {
+        String[] arregloLista = texto.split("(\\n)+");
+
+        return arregloLista;
     }
+
+
+
 
 }
