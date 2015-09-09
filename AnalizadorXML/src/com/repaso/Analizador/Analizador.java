59bc46fdea1d049e792be978b4fdaec64a822644
juanitojones19 <hastarm@gmail.com>
2015-09-09 03:13:30 -0500

    modificaciones sin importancias

--- AnalizadorXML/src/com/repaso/Analizador/Analizador.java
+++ AnalizadorXML/src/com/repaso/Analizador/Analizador.java
@@ -2,6 +2,8 @@ package com.repaso.Analizador;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Analizador {
 
@@ -26,7 +28,8 @@ public class Analizador {
 		System.out.println("total palabras: " + parrafoMultiLinea.contarPalabras());
 		*/
 		//Archivo
-		LectorArchivo lectorArchivo = new LectorArchivo("/Users/Sintelti/Documents/Parrafos.txt");
+		//LectorArchivo lectorArchivo = new LectorArchivo("/Users/Sintelti/Documents/Parrafos.txt");
+		LectorArchivo lectorArchivo = new LectorArchivo("/Users/JC/Documents/Parrafo.txt");
 		//Parrafo parrafoArchivo = new Parrafo(lectorArchivo.obtenerTexto());
         Documento documento = new Documento(lectorArchivo.obtenerTexto());
         System.out.println("Numero de Parrafos: " + documento.contarParrafos());
@@ -35,8 +38,49 @@ public class Analizador {
         System.out.println("Numero de vocales: " + documento.contarVocales());
         System.out.println("Numero de articulos: " + documento.contarArticulos());
 
-        LectorArchivo leerLista = new LectorArchivo("/Users/Sintelti/Documents/palabrasConocidas.txt");
+        LectorArchivo leerLista = new LectorArchivo("/Users/JC/Documents/palabrasConocidas.txt");
         Lista listaConocidas = new Lista(leerLista.obtenerTexto());
+
+		//lista de prueba
+		List<String> lista = new ArrayList<String>();
+
+		lista.add("hola");
+		lista.add("como");
+		lista.add("no");
+		lista.add("si");
+		lista.add("yo");
+		lista.add("la");
+
+		String palabra = "hola";
+		String palabra2 = listaConocidas.obtenerLista().get(0);
+
+		System.out.println("Palabra: " + palabra + " Palabra2: " + palabra2);
+		System.out.println("Son iguales: " + (palabra2.contains(palabra)));
+		/*
+
+		for(int i = 0; i < listaConocidas.obtenerLista().size(); i++)
+		{
+			System.out.println("Elemento: " + lista.get(i));
+		}
+
+
+		for(int i = 0; i < listaConocidas.obtenerLista().size(); i++)
+		{
+			//System.out.println("Elemento: " + listaConocidas.obtenerLista().get(i).toString());
+
+			if(listaConocidas.estaElemento("hola")) {
+				System.out.println("Si se encuenta " + lista.get(i));
+			}
+		}
+		*/
+
+
+		System.out.println("ESta?: " + listaConocidas.estaElemento("hola"));
+		System.out.println("ESta?: " + listaConocidas.estaElemento("como"));
+		System.out.println("ESta?: " + listaConocidas.estaElemento("no"));
+		System.out.println("ESta?: " + listaConocidas.estaElemento("si"));
+		System.out.println("ESta?: " + listaConocidas.estaElemento("yo"));
+		System.out.println("ESta?: " + listaConocidas.estaElemento("la"));
 
 	}// fin de main
 
